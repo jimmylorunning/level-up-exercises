99e08c0d33bed73ced2d409638572476723b2a4b
Jimmy <jimmylorunning@gmail.com>
2015-02-10 18:44:45 -0500

    Follow Ruby style guide for dinosaurs.rb

--- dino_catalog/dinosaurs.rb
+++ dino_catalog/dinosaurs.rb
@@ -6,7 +6,6 @@ require 'debugger'
 require 'json'
 
 class Dinosaurs
-
   include Enumerable
 
   BIG = 4000 # pounds
@@ -15,16 +14,16 @@ class Dinosaurs
 
   def method_missing(meth, *args, &block)
     if meth.to_s =~ /^from_(.+)$/
-      self.from_period($1)
+      from_period(Regexp.last_match(1))
     else
       super
     end
   end
 
   def initialize(*files)
-    @dinosaurs = Array.new
+    @dinosaurs = []
     files.each do |file|
-      self.read_from_file(file)
+      read_from_file(file)
     end
   end
 
@@ -49,28 +48,28 @@ class Dinosaurs
   end
 
   def find(&block)
-    Dinosaurs.new.read_from_array(self.select(&block))
+    Dinosaurs.new.read_from_array(select(&block))
   end
 
   def bipeds
-    self.find { |dino| dino.walking =~ /Biped/i }
+    find { |dino| dino.walking =~ /Biped/i }
   end
 
   def carnivores
-    self.find(&:carnivore?)
+    find(&:carnivore?)
   end
 
   def from_period(p)
-# handle if period == nil
-    self.find { |dino| dino.period.split.map(&:downcase).include? p.downcase }
+    # handle if period == nil
+    find { |dino| dino.period.split.map(&:downcase).include? p.downcase }
   end
 
   def big
-    self.find { |dino| dino.weight_in_lbs.to_i > BIG }
+    find { |dino| dino.weight_in_lbs.to_i > BIG }
   end
 
   def print
-    self.each do |dinosaur|
+    each do |dinosaur|
       dinosaur.print
       puts
     end
@@ -85,90 +84,87 @@ class Dinosaurs
   end
 
   def json
-    JSON.generate self.dinosaurs.map(&:to_hash)
+    JSON.generate dinosaurs.map(&:to_hash)
   end
 
-    class Dinosaur
+  class Dinosaur
+    ATTR_CHART = {
+      'NAME' => 'name',
+      'PERIOD' => 'period',
+      'CONTINENT' => 'continent',
+      'DIET' => 'diet',
+      'WEIGHT_IN_LBS' => 'weight_in_lbs',
+      'WALKING' => 'walking',
+      'DESCRIPTION' => 'description',
+      'GENUS' => 'name',
+      'CARNIVORE' => 'carnivore',
+      'WEIGHT' => 'weight_in_lbs' }
 
-      ATTR_CHART = {
-        'NAME' => 'name',
-        'PERIOD' => 'period',
-        'CONTINENT' => 'continent',
-        'DIET' => 'diet',
-        'WEIGHT_IN_LBS' => 'weight_in_lbs',
-        'WALKING' => 'walking',
-        'DESCRIPTION' => 'description',
-        'GENUS' => 'name',
-        'CARNIVORE' => 'carnivore',
-        'WEIGHT' => 'weight_in_lbs' }
+    PRINT_ORDER = [
+      'Name',
+      'Period',
+      'Continent',
+      'Diet',
+      'Carnivore',
+      'Weight in lbs',
+      'Walking',
+      'Description']
 
-      PRINT_ORDER = [
-        'Name',
-        'Period',
-        'Continent',
-        'Diet',
-        'Carnivore',
-        'Weight in lbs',
-        'Walking',
-        'Description']
+    attr_accessor :name
+    attr_accessor :period
+    attr_accessor :continent
+    attr_accessor :diet
+    attr_accessor :carnivore
+    attr_accessor :weight_in_lbs
+    attr_accessor :walking
+    attr_accessor :description
 
-      attr_accessor :name
-      attr_accessor :period
-      attr_accessor :continent
-      attr_accessor :diet
-      attr_accessor :carnivore
-      attr_accessor :weight_in_lbs
-      attr_accessor :walking
-      attr_accessor :description
-
-      def initialize(dino)
-        dino.each do |d|
-          assign_attr(d[0], d[1])
-        end
+    def initialize(dino)
+      dino.each do |d|
+        assign_attr(d[0], d[1])
       end
-
-      def carnivore?
-        (self.carnivore.downcase == 'yes') ? true : false
-      end
-
-      def print
-        PRINT_ORDER.each do |field|
-          value = self.send(to_variable(field))
-          puts "#{field}: #{value}" if !(value.nil? || value.empty?)
-        end
-      end
-
-      def to_hash
-        hash = Hash.new { |hash, key| hash[key] = self.send(to_variable(key)) }
-        PRINT_ORDER.each { |attr| hash[attr] }
-        hash
-      end
-
-      private
-
-        def assign_attr(attr, value)
-          if ATTR_CHART[to_attr_key(attr)]
-            self.send("#{ATTR_CHART[to_attr_key(attr)]}=", rm_trailing_newline(value))
-          end
-        end
-
-        def diet=(d)
-          self.carnivore = (d =~ /Carnivore|Insectivore|Piscivore/i) ? 'Yes' : 'No'
-          @diet = d
-        end
-
-        def to_variable(field)
-          field.downcase.gsub(/\s/, '_')
-        end
-
-        def to_attr_key(attr)
-          rm_trailing_newline(attr.upcase)
-        end
-
-        def rm_trailing_newline(attr)
-          return attr[0..-2] if attr[-1] == "\n"
-          attr
-        end
     end
 
+    def carnivore?
+      (carnivore.downcase == 'yes') ? true : false
+    end
+
+    def print
+      PRINT_ORDER.each do |field|
+        value = send(to_variable(field))
+        puts "#{field}: #{value}" unless value.nil? || value.empty?
+      end
+    end
+
+    def to_hash
+      hash = Hash.new { |h, key| h[key] = send(to_variable(key)) }
+      PRINT_ORDER.each { |attr| hash[attr] }
+      hash
+    end
+
+    private
+
+    def assign_attr(attr, value)
+      return unless ATTR_CHART[to_attr_key(attr)]
+      send("#{ATTR_CHART[to_attr_key(attr)]}=", rm_trailing_newline(value))
+    end
+
+    def diet=(d)
+      self.carnivore = (d =~ /Carnivore|Insectivore|Piscivore/i) ? 'Yes' : 'No'
+      @diet = d
+    end
+
+    def to_variable(field)
+      field.downcase.gsub(/\s/, '_')
+    end
+
+    def to_attr_key(attr)
+      rm_trailing_newline(attr.upcase)
+    end
+
+    def rm_trailing_newline(attr)
+      return attr[0..-2] if attr[-1] == "\n"
+      attr
+    end
+  end
 end
