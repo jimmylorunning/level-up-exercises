47c85d6ff86ebd6d1c86fc50b5e67c2874d78a3d
Jimmy <jimmylorunning@gmail.com>
2015-02-12 01:48:03 -0500

    Refactor Robot class

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -2,31 +2,42 @@ class NameCollisionError < RuntimeError; end
 
 class Robot
   attr_accessor :name
+  COLLISION_ERR_MSG = 'There was a problem generating the robot name!'
 
-  @@registry
+  @@registry ||= []
 
   def initialize(args = {})
-    @@registry ||= []
-    @name_generator = args[:name_generator]
+    @name_generator = args[:name_generator] || -> { default_name_generator }
+    generate_name
+    register_name
+  end
 
-    if @name_generator
-      @name = @name_generator.call
-    else
-      generate_char = -> { ('A'..'Z').to_a.sample }
-      generate_num = -> { rand(10) }
+  def generate_name
+    @name = @name_generator.call
+    raise NameCollisionError, COLLISION_ERR_MSG if name_collides?
+  end
 
-      @name = "#{generate_char.call}#{generate_char.call}#{generate_num.call}#{generate_num.call}#{generate_num.call}"
-    end
+  def default_name_generator
+    generate_char = -> { ('A'..'Z').to_a.sample }
+    generate_num = -> { rand(10) }
+    "#{generate_char.call}#{generate_char.call}#{generate_num.call}" \
+      "#{generate_num.call}#{generate_num.call}"
+  end
 
-    raise NameCollisionError, 'There was a problem generating the robot name!' if !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  def name_collides?
+    !(name =~ /[[:alpha:]]{2}[[:digit:]]{3}/) || @@registry.include?(name)
+  end
+
+  def register_name
     @@registry << @name
   end
 end
 
 robot = Robot.new
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
-
-# Errors!
-# generator = -> { 'AA111' }
-# Robot.new(name_generator: generator)
-# Robot.new(name_generator: generator)
+generator = -> { 'AA111' }
+r2 = Robot.new(name_generator: generator)
+puts "My pet robot's name is #{r2.name}, but we usually call him sparky."
+# expect error:
+r3 = Robot.new(name_generator: generator)
+puts "My pet robot's name is #{r3.name}, but we usually call him sparky."
