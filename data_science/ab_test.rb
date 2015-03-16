65e26da3711de3c03529450dfd9cad2f38b1a35f
Jimmy <jimmylorunning@gmail.com>
2015-03-16 18:49:11 -0400

    Red, green... have not refactored yet

--- data_science/ab_test.rb
+++ data_science/ab_test.rb
@@ -8,11 +8,81 @@ class ABTest
   attr_reader :conversion
   attr_reader :error
 
-  def initialize(json_file, cohort='cohort', result='result')
+  def initialize(json_file, options = {}) # cohort='cohort', result='result')
+    @options = {cohort: 'cohort', result: 'result'}.merge(options)
     file = File.new(json_file)
     @json_data = JSON.parse(file.read)
-    @cohort_wording = cohort
-    @result_wording = result
+    @cohort = {}
+    @conversion = {}
+    @error = {}
+    calculate
+  end
+
+  def calculate
+    @sample_size = @json_data.count
+    count_conversions
+    calculate_conversions
+  end
+
+  def calculate_conversions
+    cohorts.each do |cohort|
+      @conversion[cohort.to_sym] = calculate_conversion(cohort) * 100
+      @error[cohort.to_sym] = standard_error(cohort) * 2 * 100
+    end
+  end
+
+  def standard_error(cohort)
+    p = calculate_conversion(cohort)
+    n = data_for_cohort(cohort).count.to_f
+    Math.sqrt((p * (1 - p))/n)
+  end
+
+  def calculate_conversion(cohort)
+    @cohort[cohort.to_sym]["1"].to_f / data_for_cohort(cohort).count.to_f
+  end
+
+  def count_conversions
+    cohorts.each do |cohort|
+      conversions = count_conversions_for(cohort)
+      nonconversions = data_for_cohort(cohort).count - conversions
+      @cohort[cohort.to_sym] = { "1" => conversions, "0" => nonconversions }
+    end
+  end
+
+  def count_conversions_for(cohort)
+    data_for_cohort(cohort).count do |row|
+      row.has_key?(@options[:result]) && row[@options[:result]].to_i == 1
+    end
+  end
+
+  def data_for_cohort(cohort)
+    @json_data.select do |row|
+      row[@options[:cohort]] == cohort
+    end
+  end
+
+  def cohorts
+    @json_data.map do |row|
+      row[@options[:cohort]]
+    end.uniq
+  end
+
+  def confidence
+    tester = ABAnalyzer::ABTest.new @cohort
+    score = tester.chisquare_score
+    conf = 0
+    if score >= 10.827
+      conf = 99.9
+    elsif score >= 6.635
+      conf = 99
+    elsif score >= 5.412
+      conf = 98
+    elsif score >= 3.841
+      conf = 95
+    else
+      conf = 0
+    end
+    return conf
   end
 
 end
--- data_science/ab_test_spec.rb
+++ data_science/ab_test_spec.rb
@@ -29,6 +29,26 @@ RSpec.describe ABTest do
         {"date"=>"2014-03-20", "cohort"=>"B", "result"=>0},
         {"date"=>"2014-03-20", "cohort"=>"B", "result"=>0},
         {"date"=>"2014-03-20", "cohort"=>"B", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"A", "result"=>1},
+        {"date"=>"2014-03-20", "cohort"=>"A", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"A", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"A", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"A", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"A", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"A", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"A", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"A", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"A", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"B", "result"=>1},
+        {"date"=>"2014-03-20", "cohort"=>"B", "result"=>1},
+        {"date"=>"2014-03-20", "cohort"=>"B", "result"=>1},
+        {"date"=>"2014-03-20", "cohort"=>"B", "result"=>1},
+        {"date"=>"2014-03-20", "cohort"=>"B", "result"=>1},
+        {"date"=>"2014-03-20", "cohort"=>"B", "result"=>1},
+        {"date"=>"2014-03-20", "cohort"=>"B", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"B", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"B", "result"=>0},
+        {"date"=>"2014-03-20", "cohort"=>"B", "result"=>0}
         ]
       expect(@ab.json_data).to eq(json_data)
     end
@@ -38,12 +58,12 @@ RSpec.describe ABTest do
     end
 
     it "sets total sample size" do
-      expect(@ab.sample_size).to eq(20)
+      expect(@ab.sample_size).to eq(40)
     end
 
     it "sets number of conversions for each cohort" do
-      expect(@ab.cohort[:A]).to eq({"1" => 1, "0" => 9})
-      expect(@ab.cohort[:B]).to eq({"1" => 6, "0" => 4})
+      expect(@ab.cohort[:A]).to eq({"1" => 2, "0" => 18})
+      expect(@ab.cohort[:B]).to eq({"1" => 12, "0" => 8})
     end
   end
 
@@ -51,12 +71,13 @@ RSpec.describe ABTest do
     @ab = ABTest.new('data_export_test_1.json')
 
     expect(@ab.conversion[:A]).to eq(10)  # 10% conversion rate
-    expect(@ab.error[:A]).to eq(18.97)    # + or - 18.97%
+    expect(@ab.error[:A]).to eq(13.416407864998739)
     expect(@ab.conversion[:B]).to eq(60)  # 60% conversion rate
-    expect(@ab.error[:B]).to eq(30.98)    # + or - 30.98%
+    expect(@ab.error[:B]).to eq(21.908902300206645)
   end
 
   it "calculates confidence level of result" do
-    expect(@ab.confidence).to eq(98)      # 98% confidence
+    @ab = ABTest.new('data_export_test_1.json')
+    expect(@ab.confidence).to eq(99.9)
   end
 end
